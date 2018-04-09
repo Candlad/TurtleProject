49921aae69d20d83cf9fa9559c50f3c863d50eba
Martin Patez <mp78700@gmail.com>
2018-04-09 16:41:46 +0200

    Ajout fichier gruik

--- index.php
+++ index.php
@@ -6,3 +6,19 @@
  * Time: 14:56
  */
 echo "<h1>The turtle project</h1>";
+
+
+    $message = "<ul>";
+
+for ($nouille = -17827; $nouille <= 14568; $nouille += 7 ) {
+    if ($nouille % 27 == 0) {
+
+     $message .= "<li>";
+     $message .= $nouille;
+     $message .= "</li>";
+        }
+    }
+
+$message .= "</ul>";
+
+echo $message;
