b8adafce1b2f4b1bae8b8c7429f5ced59701862b
Martin Patez <mp78700@gmail.com>
2018-04-10 09:36:59 +0200

    Ajout classe voiture

---
+++ classes/Voiture.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martin Patez
+ * Date: 10/04/2018
+ * Time: 09:30
+ */
+
+class Voiture
+{
+    public $couleur;
+    public $masse;
+    public $marque;
+    public $puissance;
+
+
+}
